f3825e959e7d64b2189baa6fa24ef6df65a43b38
Jesús Enrique <jesusefg12@gmail.com>
2024-07-01 15:41:17 -0400

    Update event bus tests, considering multi thread environment

--- tests/events.py
+++ tests/events.py
@@ -1,17 +1,19 @@
 import unittest
 from dauto.utils import Event, EventBus
+import queue
 
 
 class Events(unittest.TestCase):
     eb = EventBus()
+    q = queue.Queue()
 
     def test_events_topic_matching(self):
         @self.eb.subscribe(r'test.*')
         def event_handler(_):
-            raise Exception()
+            self.q.put(True)
 
-        with self.assertRaises(Exception):
-            self.eb.dispatch(Event("test.data", {}))
+        self.eb.dispatch(Event("test.data", {}))
+        assert self.q.get(timeout=5)
 
 
 if __name__ == '__main__':
